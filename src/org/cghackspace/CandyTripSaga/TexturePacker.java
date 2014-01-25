f7d4fd5eaa5816071bbaaea3ad1833257b8fc39a
Thiago Santos <ts.santos@sisa.samsung.com>
2014-01-25 18:56:40 -0300

    texturepacker: helper class to pack textures

---
+++ src/org/cghackspace/CandyTripSaga/TexturePacker.java
@@ -0,0 +1,17 @@
+package org.cghackspace.CandyTripSaga;
+
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+
+public class TexturePacker {
+
+	public static void main(String[] args) {
+		TexturePacker2.process("/home/thiagoss/workspace/CandyTripSaga-android/assets/kurt/MovingR",
+				"/home/thiagoss/workspace/CandyTripSaga-android/assets/",
+				"kurttextures.pack");
+	}
+
+}
+
+
+
+ 
