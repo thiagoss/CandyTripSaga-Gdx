5af2f2a36907894cb83af5ee0f2dac67f2ec5e9f
Thiago Santos <ts.santos@sisa.samsung.com>
2014-01-25 15:47:13 -0300

    kid: switch to using actor

--- src/org/cghackspace/CandyTripSaga/GameScreen.java
+++ src/org/cghackspace/CandyTripSaga/GameScreen.java
@@ -3,46 +3,36 @@ package org.cghackspace.CandyTripSaga;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class GameScreen implements Screen {
 
 	final CandyTripSaga game;
-	private OrthographicCamera camera;
+	Stage stage;
 	private Kid kid;
 	
 	public GameScreen(CandyTripSaga game) {
 		this.game = game;
 		
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, CandyTripSaga.WINDOW_WIDTH,
-				CandyTripSaga.WINDOW_HEIGHT);
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
 		
 		kid = new Kid();
-		kid.setPosition(20, CandyTripSaga.WINDOW_HEIGHT/2);
+		stage.addActor(kid);
+		stage.setKeyboardFocus(kid);
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		camera.update();
-		
-		game.batch.setProjectionMatrix(camera.combined);
-		
-		game.batch.begin();
-		kid.draw(game.batch);
-		game.batch.end();
-		
-		kid.handleInput();
-		kid.clampToBounds(0, 0, CandyTripSaga.WINDOW_WIDTH,
-				CandyTripSaga.WINDOW_HEIGHT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
+		stage.setViewport(CandyTripSaga.WINDOW_WIDTH,
+				CandyTripSaga.WINDOW_HEIGHT, false);
 
 	}
 
@@ -72,8 +62,8 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void dispose() {
+		stage.dispose();
 		kid.dispose();
-
 	}
 
 }
--- src/org/cghackspace/CandyTripSaga/Kid.java
+++ src/org/cghackspace/CandyTripSaga/Kid.java
@@ -2,49 +2,56 @@ package org.cghackspace.CandyTripSaga;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 
-public class Kid {
+public class Kid extends Actor {
 
-	private static final int MOVEMENT = 10;
-	int x;
-	int y;
+	private static final float MOVEMENT = 5f;
+	
 	Texture image;
 	
 	public Kid() {
-		image = new Texture(Gdx.files.internal("kid.png"));
+		image = new Texture(Gdx.files.internal("kurt_64x64_02.png"));
+		this.setBounds(0, 0, image.getWidth(), image.getHeight());
 	}
 	
-	public void setPosition(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public void draw (SpriteBatch batch, float parentAlpha) {
+		batch.draw(image, getX(), getY());
+    }
+	
+	public void act(float delta) {
+		super.act(delta);
+		handleInput();
+		clampToBounds(0, 0, getStage().getWidth(), getStage().getHeight());
 	}
-
-	public void draw(SpriteBatch batch) {
-		batch.draw(image, x, y);
-	}
-
+	
+	
 	public void handleInput() {
 		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			x += MOVEMENT;
+			setX(getX() + MOVEMENT);
 		} else if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			x -= MOVEMENT;
+			setX(getX() - MOVEMENT);
 		}
 		
 		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-			y -= MOVEMENT;
+			setY(getY() - MOVEMENT);
 		} else if(Gdx.input.isKeyPressed(Keys.UP)) {
-			y += MOVEMENT;
+			setY(getY() + MOVEMENT);
 		}	
 	}
 
-	public void clampToBounds(int x0, int y0, int x1, int y1) {
-		if (x < x0) x = x0;
-		else if (x + image.getWidth() > x1) x = x1 - image.getWidth();
+	public void clampToBounds(float x0, float y0, float x1, float y1) {
+		if (getX() < x0) setX(x0);
+		else if (getX() + image.getWidth() > x1) setX(x1 - image.getWidth());
 		
-		if (y < y0) y = y0;
-		else if (y + image.getHeight() > y1) y = y1 - image.getHeight();
+		if (getY() < y0) setY(y0);
+		else if (getY() + image.getHeight() > y1) setY(y1 - image.getHeight());
 	}
 
 	public void dispose() {
